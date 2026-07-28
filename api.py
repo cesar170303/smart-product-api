c0278bf6fc48f8ae28c0b703e80296177b4c8b96
César Martínez Dueñas <cesar.mar.due@gmail.com>
2026-07-28 13:29:34 +0200

    refactor: inyectar sesion de base de datos con Depends y actualizar GET y POST

--- api.py
+++ api.py
@@ -1,24 +1,44 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from main import DataPersistence
 from models import ProductModel
-from sqlmodel import SQLModel
-from database import engine
+from sqlmodel import SQLModel, Session, select
+from database import engine, get_session
+from contextlib import asynccontextmanager
 
 
-app = FastAPI()
+
 datapersistence = DataPersistence()
 
-@app.on_event("startup")
-def create_db_and_tabes():
+#async: significa que está diseñado para hacer varias cosas a la vez sin quedarse bloqueado
+#Este decorador es una herramienta que coge una función normal y corriente, busca la palabra yield,
+#  y automáticamente construye esa clase por ti por debajo. Convierte lo que está antes del yield en el __enter__ y lo que está después en el __exit__.
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     print("Conectando a la base de datos y creando tablas...")
     SQLModel.metadata.create_all(engine)
 
+    #la función pausa su ejecución y le devuelve el control al Event Loop de FastAPI/Uvicorn.
+    yield
+    #Esto se acabará ejecutando cuando cerramos el servidor
+    print("Cerrando las instalaciones...")
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/products")
-def products():
+def products(session: Session = Depends(get_session)):
+    """ 1. Construimos la pregunta select(ProductModel)
+    Dame toda la información de la tabla ProductModel.
+    2. Ejecutamos la pregunta contra la base de datos
+    .session.exec() envía la orden a Postgres.
+    .all() le dice: Tráeme todos los resultados de golpe en una lista de Python
+    """
+    statment = select(ProductModel)
 
-    return datapersistence.load_products()
+    results = session.exec(statment).all()
+
+    return results
 
 def get_ai_category(product_name:str) -> str:
     print(f"Analizando el producto {product_name}")
@@ -34,19 +54,23 @@ def get_ai_category(product_name:str) -> str:
 
 
 @app.post("/add_products")
-def add_products(new_product: ProductModel):
+def add_products(new_product: ProductModel, session: Session = Depends(get_session)):
     
-    actual_list = datapersistence.load_products()
+    #actual_list = datapersistence.load_products()
 
     category_ai = get_ai_category(new_product.name)
     new_product.category = category_ai
-
     new_product.apply_pricing_rules()
 
-    dict_product = new_product.model_dump()
+    session.add(new_product)
+    session.commit()
+    session.refresh(new_product)
+
+    """dict_product = new_product.model_dump()
     actual_list.append(dict_product)
 
-    datapersistence.save_product(actual_list)
+    datapersistence.save_product(actual_list)"""
+
     return {"mensaje": "Producto añadido correctamente", "Producto" : new_product}
 
 
--- database.py
+++ database.py
@@ -1,6 +1,12 @@
-from sqlmodel import create_engine
+from sqlmodel import create_engine, Session
 
-DATABASE_URL = "postgresql://postgres:postgres$@localhost:5432/postgres"
+DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/postgres"
 
 engine = create_engine(DATABASE_URL, echo=True)
 
+
+def get_session():
+    """Gracias a esta funcionn los endpoints no sabran que base de dats hay por detrás,
+        solo saben que hay una session pero puede ser cualquier base de datos,etc"""
+    with Session(engine) as session:
+        yield session

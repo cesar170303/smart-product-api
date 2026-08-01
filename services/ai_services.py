041db1787960d552e17298767bf289eef7cffc4c
César Martínez Dueñas <cesar.mar.due@gmail.com>
2026-08-01 11:51:47 +0200

    feat: integracion completa de IA para categorizacion automatica

--- services/ai_services.py
+++ services/ai_services.py
@@ -1,19 +1,28 @@
 from dotenv import load_dotenv
 import os
+from google import genai
 
 load_dotenv()
 
-# Accede a las variables de entorno
-secret_key= os.getenv("GEMINI_API_KEY")
+client = genai.Client()
+
 
 def get_ai_category(product_name: str) -> str:
     print(f"Analizando el producto {product_name}")
+
+    prompt = f"""
+            Persona: Eres un calificador de productos de gran experiencia con 15 años.
+            Contexto: Necesitamos indicar una categoria para el producto.
+            Tarea: Clasifica el producto y devolvemos una palabra solamente, el producto a calificar es {product_name}.
+            Ejemplo y restricciones: Solo necesitamos UNA palabra que sera la CATEGORIA ,ejemplo:Lácteos, Limpieza, Fruta, Tecnolgia, etc. No queremos
+            que sea una frase nada de eso solo UNA palabra.
+        """
+
+    response = client.interactions.create(
+        model = "gemini-3.5-flash",
+        input = prompt
+    )
     
-    lower_name = product_name.lower()
-    if "mancuerna" in lower_name or "bici" in lower_name:
-        return "Deporte"
-    elif "movil" in lower_name or "tablet" in lower_name:
-        return "Tecnologia"
-    else:
-        return "General"
+    
+    return response.output_text.strip()
 
